269a7c46c415748c23440f638680d67748ab78db
Peter Lebedev <LPeter@mail.ru>
2021-04-15 15:08:14 +0300

    All done

--- server.js
+++ server.js
@@ -8,12 +8,20 @@ const mongoose = require('mongoose');
 const urlFileName = `${process.cwd()}/.url`;
 const port = process.env.PORT || 3000;
 
+// log all requests - for debugging
+//app.use((req, res, next) => {
+//  console.log(`${req.method} ${req.path}, body = ${req.body}`); // req.body undefined!
+//  next();
+//});
+
+// main code
 app.use(cors());
 app.use('/public', express.static(`${process.cwd()}/public`));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/.url", function (req, res) {
-  res.send(404, 'Not Found');
+// disable access to file .env
+app.get("/.env", function (req, res) {
+  res.send(403, 'Access denied');
 });
 
 app.get('/', function (req, res) {
@@ -30,11 +38,6 @@ mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology
   .catch(err => {
     console.error('Cannot connect to mongoDB', err);
   });
-//const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function () {
-//   // we're connected!
-// });
 
 // define  a schema for URL shortener
 const schema = new mongoose.Schema({
@@ -43,11 +46,11 @@ const schema = new mongoose.Schema({
 });
 const model = mongoose.model('ShortUrl', schema);
 
-// 
+// создание нового шорта или вернуть уже существующий, если есть
 app.post('/api/shorturl', function (req, res) {
   let longUrl = req.body.url;
   if (!longUrl || longUrl.length < 2) {
-    return res.json({ error: 'invalid url'});
+    return res.json({ error: 'invalid URL' });
   }
   // check URL via dns lookup
   if (!/^https?/.test(longUrl)) {
@@ -68,7 +71,6 @@ app.post('/api/shorturl', function (req, res) {
         const newUrl = model({ long_url: longUrl, short_url: "" }); // now we got generated field "_id"
         newUrl.short_url = newUrl._id.toString().slice(-6);
         newUrl.save(function (err) {
-          console.log("the new URL is added");
           res.json({ original_url: longUrl, short_url: newUrl.short_url });
         });
       } else {
@@ -78,70 +80,10 @@ app.post('/api/shorturl', function (req, res) {
   }); // dns.resole()
 }); // app.post()
 
-// generate short url for POST
-// app.post("/api/shorturl/new", function (req, res) {
-//   // значение URL берем из запроса - req.body.url, проверяем через dns
-//   try {
-//     let inputURL = req.body.url;
-//     console.log('req body=',req.body);
-//     // случай, если указан "example.com", а не "http://example.com"
-//     // для http и https - разные короткие URL.
-//     // если не указать http://, то URL() выдаст ошибку
-//     if (!/^https?/.test(inputURL)) {
-//       inputURL = "http://" + inputURL;
-//     }
-//     let urlObj = new URL(inputURL);
-//     dns.resolve(urlObj.host, function (err, address) {
-//       if (err) {
-//         console.log('resolve error:',err);
-//         res.json({ error: 'invalid url' });
-//       } else {
-//         // получаем ранее сохраненные данные и добавляем новые
-//         let urls = readUrls(); // при первом запросе будет null, т.к. файла еще нет
-//         if (!urls) {
-//           urls = {};
-//         }
-//         let shortURL = Object.values(urls).length ? Math.max(...Object.values(urls)) + 1 : 1;
-//         urls[inputURL] = shortURL;
-//         saveUrls(urls);
-//         let ret = { original_url: inputURL, short_url: shortURL };
-//         console.log('sending:',ret);
-//         res.json(ret);
-//       }
-//     });
-//   } catch (e) {
-//     console.error('post error', e);
-//     res.json({ error: 'invalid url' });
-//   }
-// });
-
-// сделать редирект по короткому пути
-// app.get("/api/shorturl/:url", function (req, res) {
-//   let url = req.params.url;
-//   console.log('found url', url);
-//   if (!url) {
-//     return res.json({ error: 'invalid url' });
-//   }
-//   try {
-//     let urls = readUrls();
-//     // находим в объекте имя свойства, значение которого = url
-//     let u = Object.entries(urls).find(e => e[1] == url);
-//     console.log('found ', u);
-//     if (!u) {
-//       throw new Error('INVALID URL');
-//     }
-//     console.log('redirect to', u);
-//     res.redirect(u[0]);
-//   } catch (e) {
-//     console.error(e);
-//     res.json({ error: 'invalid url' });
-//   }
-// });
 
 app.get("/api/shorturl/:url", function (req, res) {
   let url = req.params.url;
   if (!url || url.length != 6) {
-    console.log('get error with url', url);
     res.json({ error: 'invalid URL' });
   } else {
     // найти в БД url по short_url
@@ -149,8 +91,6 @@ app.get("/api/shorturl/:url", function (req, res) {
       if (err) {
         res.json({ error: 'invalid URL' });
       } else {
-        console.log('return data:', data);
-        //res.json({ original_url: data.long_url, short_url: data.short_url });
         res.redirect(data.long_url);
       }
     }); // findOne()
